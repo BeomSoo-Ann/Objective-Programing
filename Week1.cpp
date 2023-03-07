333ef51b6a3b5e00678f0f9dfb445a2987601149
BeomSoo_Ann <68950105+BeomSoo-Ann@users.noreply.github.com>
2023-03-07 14:04:40 +0900

    Week1

---
+++ Week1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+/*Include directives, #include를 통해 Library를 가져옴*/
+
+int main() /*Integer type의 output을 출력하는 main 함수, 보통 integer나 void를 많이 사용*/
+{          /*Program statements, 코드의 Body part*/
+    int x;
+    x = 10;
+}
