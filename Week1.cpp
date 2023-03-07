a189d139b93b83e492b8cb82215c5bb3880f88f6
BeomSoo_Ann <68950105+BeomSoo-Ann@users.noreply.github.com>
2023-03-07 14:42:00 +0900

    Week1

--- Week1.cpp
+++ Week1.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 /*Include directives, #include를 통해 Library를 가져옴*/
 
+using namespace std;
+
 int main() /*Integer type의 output을 출력하는 main 함수, 보통 integer나 void를 많이 사용*/
-{          /*Program statements, 코드의 Body part*/
-    int x;
-    x = 10;
+
+/*Program statements, 코드의 Body part*/
+
+{
+    int x1 = 10;
+    int x2, y1, z1;
+    int x3 = 0, y2, z2 = 5;
+    long x4 = 4456;
+
+    double pi = 3.14159;
+
+    char ch1, ch2;
+    /*ASCII Code number*/
+    ch1 = 65;
+    /*ASCII Code*/
+    ch2 = 'A';
+
+    const double PI = 3.14159;
+    /*CONST는 ALL CAPITAL LATTER 사용 권장*/
+    double temp = 0;
+
+    temp = PI;
+
+    cout << "Print Anything";
+    /*Python의 print 역할을 하는 녀석*/
+    int value1, value2, sum;
+    cin >> value1 >> value2;
+    sum = value1 + value2;
+    /*Python의 input 역할을 하는 녀석*/
 }
