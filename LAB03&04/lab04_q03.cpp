ec64ce007fcb4fe1d7a56f7530536da90ff3ef88
BeomSoo_Ann <68950105+BeomSoo-Ann@users.noreply.github.com>
2023-03-19 17:36:33 +0900

    Update lab04_q03

--- LAB03&04/lab04_q03.cpp
+++ LAB03&04/lab04_q03.cpp
@@ -14,7 +14,7 @@ int main()
     cout << "Rounding up number of " << x << " is " << ceil(x) << endl;
     cout << "Rounding down number of " << x << " is " << floor(x) << endl;
     cout << "Rounding number of " << x << " is " << round(x) << endl;
-    cout << "Maximum number of " << x << " and " << y << " is " << max(x, y) << endl;
+    cout << "Maximum number between " << x << " and " << y << " is " << max(x, y) << endl;
     cout << y << "-th root of " << x << " is " << pow(x, 1 / y) << endl;
     cout << "|" << x << " - " << y << "| is " << abs(x - y) << endl;
 
